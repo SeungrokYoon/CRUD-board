2eafb96aa51b3a5ca60ec8b2ea588e20a6b187ef
Seungrok <sonicrok93@gmail.com>
2022-06-13 20:14:03 +0900

    fix: 잘못된 스타일드 컴포넌트 사용 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
 import type { ReactElement } from 'react'
-import {} from 'react'
+import styled from 'styled-components'
+
+const DivContainer = styled.div`
+  background: white;
+`
 
 const Header = (): ReactElement => {
   const names = ['Alice', 'Bob', 'Eve']
   names.forEach(name => {
     name.toUpperCase()
   })
-  return <div>Header</div>
+  return <DivContainer>Header</DivContainer>
 }
 
 export default Header
